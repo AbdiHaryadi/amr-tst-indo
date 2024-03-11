c674fde5fc49ec7a0786f721c6e402290933686a
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2024-03-12 06:23:49 +0800

    feat: add raw AMR in iterator

--- main.py
+++ main.py
@@ -322,7 +322,7 @@ def sent_amr_iter(path: str):
                         status = "find_begin_of_amr"
                     elif line.startswith(SENT_PREFIX):
                         if current_sent != "":
-                            yield current_sent, ("", "AMR is empty")
+                            yield current_sent, "", "", "AMR is empty"
                         current_sent = line[sent_offset:].strip()
                     # else: ignore
 
@@ -334,16 +334,20 @@ def sent_amr_iter(path: str):
                         current_amr = line
                         status = "select_amr_until_blank_line"
                     else:
-                        yield current_sent, ("", "AMR is empty")
+                        yield current_sent, "", "", "AMR is empty"
                         current_sent = ""
                         status = "find_end_of_header"
                     
                 case "select_amr_until_blank_line":
                     if line == "" or line.startswith("#"):
                         try:
-                            yield current_sent, (to_amr_with_pointer(current_amr), "")
+                            amr_with_pointer = to_amr_with_pointer(current_amr)
+                            if current_sent != "":
+                                yield current_sent, current_amr, amr_with_pointer, ""
+                            else:
+                                yield current_sent, current_amr, amr_with_pointer, "Sentence is empty"
                         except ValueError as e:
-                            yield current_sent, (current_amr, str(e))
+                            yield current_sent, current_amr, "", str(e)
                     
                         current_sent = ""
                         current_amr = ""
@@ -360,28 +364,29 @@ def sent_amr_iter(path: str):
             
         if status == "select_amr_until_blank_line":
             try:
-                yield current_sent, (to_amr_with_pointer(current_amr), "")
+                amr_with_pointer = to_amr_with_pointer(current_amr)
+                if current_sent != "":
+                    yield current_sent, current_amr, amr_with_pointer, ""
+                else:
+                    yield current_sent, current_amr, amr_with_pointer, "Sentence is empty"
             except ValueError as e:
-                yield current_sent, (current_amr, str(e))
+                yield current_sent, current_amr, "", str(e)
 
 def to_jsonl_dataset_2(input_path: str, output_path: str):
     error_count = 0
     with open(output_path, mode="w") as fp_out:
-        for sent, (amr, error_message) in sent_amr_iter(input_path):
-            if error_message == "" and sent != "":
+        for sent, raw_amr, amr_with_pointer, error_message in sent_amr_iter(input_path):
+            if error_message == "":
                 print(
-                    json.dumps({"sent": sent, "amr": amr, "lang": "id"}),
+                    json.dumps({"sent": sent, "amr": amr_with_pointer, "lang": "id"}),
                     file=fp_out
                 )
             else:
-                if error_message == "":
-                    error_message = "Sentence is empty"
-
                 error_count += 1
                 print(f"(Error {error_count}) {error_message}")
                 print(f"Sentence: {sent}")
                 print(f"AMR:")
-                print(amr)
+                print(raw_amr)
                 print("---")
 
     if error_count > 0:
