f115a402ab9b2d71468487567ebd07f4eb42ed55
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2024-05-10 08:32:32 +0800

    feat: add infos in AMRTST call

--- amr_tst.py
+++ amr_tst.py
@@ -19,50 +19,25 @@ class AMRTST:
         self.sr = sr
 
     def __call__(self, texts: list[str], source_styles: list[str]):
-        print("Warning: because AMR Generator is not implemented yet, the return is graphs.")
+        print("Warning: AMR Generator is not implemented, returning (list of empty string, infos)")
         graphs = self.t2a(texts)
 
-        target_graphs: list[penman.Graph] = []
+        infos: dict[list] = {
+            "source_amr": [penman.encode(g) for g in graphs],
+            "style_words": [],
+            "target_amr": []
+        }
+
+        target_texts: list[str] = []
+
         for t, g, s in zip(texts, graphs, source_styles):
             current_style_words = self.sd(t)
+            infos["style_words"].append(current_style_words)
+
             g_tgt = self.sr(t, g, s, current_style_words)
-            target_graphs.append(g_tgt)
+            infos["target_amr"].append(penman.encode(g_tgt))    
 
-        return target_graphs
-
-    @staticmethod
-    def load(
-            t2a_model_name: str,
-            sd_config_path: str,
-            sd_model_path: str,
-            sr_style_clf_hf_checkpoint: str,
-            sr_fasttext_model_path: str,
-            *,
-            t2a_hf_repo_id: str | None = "",
-            t2a_hf_kwargs: dict = {},
-            t2a_kwargs: dict = {},
-            sd_kwargs: dict = {},
-            sr_kwargs: dict = {}
-    ):
-        if t2a_hf_repo_id is None:
-            t2a = TextToAMR(model_name=t2a_model_name, **t2a_kwargs)
-        else:
-            t2a = TextToAMR.from_huggingface(
-                t2a_hf_repo_id,
-                model_name=t2a_model_name,
-                hf_kwargs=t2a_hf_kwargs,
-                **t2a_kwargs
-            )
+            # TODO: AMR Generator here.
+            target_texts.append("")
         
-        sd = StyleDetector(
-            config_path=sd_config_path,
-            model_path=sd_model_path,
-            **sd_kwargs
-        )
-        sr = StyleRewriting(
-            style_clf_hf_checkpoint=sr_style_clf_hf_checkpoint,
-            fasttext_model_path=sr_fasttext_model_path,
-            **sr_kwargs
-        )
-
-        return AMRTST(t2a=t2a, sd=sd, sr=sr)
+        return target_texts, infos
