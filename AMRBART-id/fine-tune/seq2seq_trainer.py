53f683d232fd9a23ecf4ee57a6be366e6d6786d9
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2024-06-27 15:05:41 +0800

    fix: remove do_grad_scaling in Seq2SeqTrainer

--- AMRBART-id/fine-tune/seq2seq_trainer.py
+++ AMRBART-id/fine-tune/seq2seq_trainer.py
@@ -212,9 +212,7 @@ class Seq2SeqTrainer(Trainer):
             # deepspeed handles loss scaling by gradient_accumulation_steps in its `backward`
             loss = loss / self.args.gradient_accumulation_steps
 
-        if self.do_grad_scaling:
-            self.scaler.scale(loss).backward()
-        elif self.use_apex:
+        if self.use_apex:
             with amp.scale_loss(loss, self.optimizer) as scaled_loss:
                 scaled_loss.backward()
         elif self.deepspeed:
