78aaffef630c5ff417524d30d2bf449f1c5e442b
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2024-07-24 10:34:07 +0800

    feat: use AMRToText in AMRTST class

--- amr_tst.py
+++ amr_tst.py
@@ -1,7 +1,39 @@
 import penman
+from amr_to_text import AMRToText
+from dataclasses import dataclass
 from style_detector import StyleDetector
 from style_rewriting import StyleRewriting
 from text_to_amr import TextToAMR
+
+@dataclass
+class AMRTSTDetailedResult:
+    source_text: list[str]
+    source_style: list[str]
+    source_amr: list[penman.Graph]
+    style_words: list[list[str]]
+    target_amr: list[penman.Graph]
+    target_text: list[str]
+
+    def to_list(self) -> list[dict[str, str | list[str]]]:
+        expected_length = len(expected_length)
+        assert (other_length := len(self.source_style)) == expected_length, f"Expecting {expected_length}, got {other_length}"
+        assert (other_length := len(self.source_amr)) == expected_length, f"Expecting {expected_length}, got {other_length}"
+        assert (other_length := len(self.style_words)) == expected_length, f"Expecting {expected_length}, got {other_length}"
+        assert (other_length := len(self.target_amr)) == expected_length, f"Expecting {expected_length}, got {other_length}"
+        assert (other_length := len(self.target_text)) == expected_length, f"Expecting {expected_length}, got {other_length}"
+
+        data: list[dict[str, str | list[str]]] = []
+        for i in range(expected_length):
+            data.append({
+                "source_text": self.source_text[i],
+                "source_style": self.source_style[i],
+                "source_amr": penman.encode(self.source_amr[i], indent=None),
+                "style_words": self.style_words[i],
+                "target_amr": penman.encode(self.target_amr[i], indent=None),
+                "target_text": self.target_text[i],
+            })
+
+        return data
 
 class AMRTST:
     """
@@ -12,34 +44,34 @@ class AMRTST:
             self,
             t2a: TextToAMR,
             sd: StyleDetector,
-            sr: StyleRewriting
+            sr: StyleRewriting,
+            a2t: AMRToText
     ):
         self.t2a = t2a
         self.sd = sd
         self.sr = sr
+        self.a2t = a2t
 
     def __call__(self, texts: list[str], source_styles: list[str]):
         assert len(texts) == len(source_styles)
-
-        print("Warning: AMR Generator is not implemented, returning (list of empty string, infos)")
         graphs = self.t2a(texts)
 
-        infos: dict[list] = {
-            "source_amr": [penman.encode(g) for g in graphs],
-            "style_words": [],
-            "target_amr": []
-        }
-
-        target_texts: list[str] = []
+        style_words_list: list[list[str]] = []
+        rewritten_graphs: list[penman.Graph] = []
 
         for t, g, s in zip(texts, graphs, source_styles):
             current_style_words = self.sd(t)
-            infos["style_words"].append(current_style_words)
+            style_words_list.append(current_style_words)
 
             g_tgt = self.sr(t, g, s, current_style_words)
-            infos["target_amr"].append(penman.encode(g_tgt))    
+            rewritten_graphs.append(g_tgt)
 
-            # TODO: AMR Generator here.
-            target_texts.append("")
-        
-        return target_texts, infos
+        target_texts = self.a2t(rewritten_graphs)
+        return target_texts, AMRTSTDetailedResult(
+            source_text=texts,
+            source_style=source_styles,
+            source_amr=graphs,
+            style_words=style_words_list,
+            target_amr=rewritten_graphs,
+            target_text=target_texts
+        )
