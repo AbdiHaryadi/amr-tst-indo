f4ea50e984011b899c62649afeeb887b6c2f30b6
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2024-08-09 16:50:31 +0800

    feat: use patched Transformers for AMRToText; remove scale arg

--- amr_to_text.py
+++ amr_to_text.py
@@ -18,12 +18,9 @@ from transformers import (
     AutoConfig,
     MBartTokenizer,
     MBartTokenizerFast,
-    # MBartForConditionalGeneration as BartForConditionalGeneration,
+    MBartForConditionalGeneration as BartForConditionalGeneration,
     set_seed
 )
-
-# This import is temporarily used.
-from modified_modeling_mbart import MBartForConditionalGeneration as BartForConditionalGeneration
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -36,7 +33,7 @@ def mkdir_if_not_exists(dir):
     if not os.path.exists(dir):
         os.mkdir(dir)
 
-def prepare_tokenizer_and_model(training_args, model_args, data_args, scale_embedding: bool = False):
+def prepare_tokenizer_and_model(training_args, model_args, data_args):
     # Set seed before initializing model.
     set_seed(training_args.seed)
 
@@ -44,8 +41,7 @@ def prepare_tokenizer_and_model(training_args, model_args, data_args, scale_embe
         model_args.model_name_or_path,
         cache_dir=model_args.cache_dir,
         revision=model_args.model_revision,
-        use_auth_token=True if model_args.use_auth_token else None,
-        scale_embedding=scale_embedding
+        use_auth_token=True if model_args.use_auth_token else None
     )
 
     tokenizer = AMRBartTokenizer.from_pretrained(
