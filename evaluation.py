7f725682b994c1d56e48d7c590d32070165ba8c8
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2024-10-17 11:09:05 +0800

    feat: add more metrics in evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,11 +1,36 @@
 import evaluate
 bleu_metric = evaluate.load("evaluate-metric/sacrebleu")
+rouge_metric = evaluate.load("rouge")
 
 def compute_metrics_generation(decoded_preds, decoded_labels):
+    return compute_bleu(decoded_preds, decoded_labels)
+
+def compute_bleu(raw_predictions, raw_references):
     # Some simple post-processing
-    predictions = [pred.strip() for pred in decoded_preds]
-    references = [[label.strip()] for label in decoded_labels] # sacrebleu uses multi reference setting
+    predictions = [pred.strip() for pred in raw_predictions]
+    references = [[label.strip()] for label in raw_references] # sacrebleu uses multi reference setting
 
     return bleu_metric.compute(
         predictions=predictions, references=references, lowercase=True
     )
+
+def compute_rouge_l(raw_predictions, raw_references):
+    return rouge_metric.compute(
+        predictions=raw_predictions, references=raw_references, rouge_types=["rougeL"]
+    )
+
+def compute_detail_bleu(raw_predictions, raw_references):
+    results = []
+    for raw_p, raw_r in zip(raw_predictions, raw_references):
+        single_result = compute_bleu([raw_p], [raw_r])
+        results.append(single_result)
+    
+    return results
+
+def compute_detail_rouge_l(raw_predictions, raw_references):
+    results = []
+    for raw_p, raw_r in zip(raw_predictions, raw_references):
+        single_result = compute_rouge_l([raw_p], [raw_r])
+        results.append(single_result)
+    
+    return results
