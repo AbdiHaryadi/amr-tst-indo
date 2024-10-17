a144c3023b87f4d13721c1386bc9a6fd45ad2b2c
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2024-10-17 10:38:05 +0800

    feat: change AMR to pointer function, using Penman

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import penman
 import re
+from typing import Callable
+
+import penman.layout
 node_name_matcher = re.compile(r"^[a-z]{1,3}([0-9]+)?$")
 
 def is_node_name(current_token):
@@ -13,7 +16,7 @@ def make_no_metadata_graph(g: penman.Graph):
         metadata={}
     )
 
-def to_amr_with_pointer(amr: str):
+def old_to_amr_with_pointer(amr: str):
     result = ""
     status = "find_first_left"
     level = 0
@@ -155,7 +158,8 @@ def to_amr_with_pointer(amr: str):
             # else: ignore
             
         elif status == "find_end_of_non_literal_value":
-            if c in "abcdefghijklmnopqrstuvwxyz-0123456789":
+            # It includes float.
+            if c in "abcdefghijklmnopqrstuvwxyz-0123456789.":
                 current_token += c
             elif c.isspace() or c == ")":
                 if is_node_name(current_token):
@@ -205,3 +209,117 @@ def to_amr_with_pointer(amr: str):
         raise ValueError(f"Unresolved node names: {unresolved_node_names}")
         
     return result
+
+def _is_z_prefix_variable(var: str):
+    if len(var) <= 1:
+        return False
+
+    if var[0] != "z":
+        return False
+    
+    return var[1:].isdigit()
+
+def _zpv_to_pointer(zp_var: str):
+    # zp stands for z-prefix
+    return f"<pointer:{zp_var[1:]}>"
+
+def to_amr_with_pointer(amr_str: str):
+    g = penman.decode(amr_str)
+
+    # Convert all variables to z-prefix
+    var_list = []
+    possibly_already_z_prefix = True
+
+    for var, _, _ in g.instances():
+        if var not in var_list:
+            if not _is_z_prefix_variable(var):
+                possibly_already_z_prefix = False
+            
+            var_list.append(var)
+
+    if possibly_already_z_prefix:
+        new_g = _convert_all_variables_to_pointer(g, _is_z_prefix_variable, _zpv_to_pointer)
+
+    else:
+        next_pointer_id = 0
+        var_to_pointer: dict[str, str] = {}
+        for var in var_list:
+            pointer = f"<pointer:{next_pointer_id}>"
+            var_to_pointer[var] = pointer
+            next_pointer_id += 1
+
+        is_variable_fn: Callable[[str], bool]  = lambda x: x in var_to_pointer
+        to_pointer_fn: Callable[[str], str] = lambda x: var_to_pointer[x]
+
+        new_g = _convert_all_variables_to_pointer(g, is_variable_fn, to_pointer_fn)
+
+    new_g_encoded = penman.encode(new_g, indent=None)
+    
+    new_amr_str = ""
+    in_literal = False
+    prev_is_ignore_char = False
+    for c in new_g_encoded:
+        if in_literal:
+            new_amr_str += c
+            if c == "\\":
+                prev_is_ignore_char = not prev_is_ignore_char
+            else:
+                if c == "\"" and (not prev_is_ignore_char):
+                    in_literal = False
+                prev_is_ignore_char = False
+        else:
+            if c == "(":
+                new_amr_str += "( "
+            elif c == ")":
+                new_amr_str += " )"
+            elif c == "/":
+                pass # Ignore this symbol.
+            elif c == " ":
+                if new_amr_str != "" and new_amr_str[-1] != " ":
+                    new_amr_str += " "
+            else:
+                new_amr_str += c
+                if c == "\"":
+                    in_literal = True
+
+    return new_amr_str
+
+def _convert_all_variables_to_pointer(
+        g: penman.Graph,
+        is_variable_fn: Callable[[str], bool],
+        to_pointer_fn: Callable[[str], str]):
+    new_triples = []
+    new_epidata = {}
+    for triple in g.triples:
+        epidata_value = g.epidata[triple]
+        src, rel, tgt = triple
+        if is_variable_fn(src):
+            src = to_pointer_fn(src)
+
+        if is_variable_fn(tgt):
+            tgt = to_pointer_fn(tgt)
+
+        new_single_triple = (src, rel, tgt)
+        new_triples.append(new_single_triple)
+
+        new_epidata_value = []
+        for evi in epidata_value:
+            if isinstance(evi, penman.layout.Push) and is_variable_fn(evi.variable):
+                new_epidata_value.append(
+                        penman.layout.Push(to_pointer_fn(evi.variable))
+                    )
+            else:
+                new_epidata_value.append(evi)
+            
+        new_epidata[new_single_triple] = new_epidata_value
+
+    new_top = g.top
+    if is_variable_fn(new_top):
+        new_top = to_pointer_fn(new_top)
+        
+    return penman.Graph(
+        triples=new_triples,
+        top=new_top,
+        epidata=new_epidata,
+        metadata=g.metadata
+    )
